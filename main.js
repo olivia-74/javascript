bf4299af08af23a3da64b921ba382d851af80dde
Olivia Dresch <84184891+olivia-74@users.noreply.github.com>
2023-03-14 12:25:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+// comentário
+/* comentario para mais texto */
+// const: não pode ter valor alterado
+// let: pode ter valor alterado
+// string: texto ""
+// tipos de variáveis: booleans, numbers, string
+// compt: interação c/ usuário
+// console.log = print
+//boolean =  console.log, prompt
+
+// console.log("Olá, Mundo!")
+
+// const nome = "Olivia"
+// console.log(nome)
+
+// let idade = 18
+// console.log(idade)
+// idade = 19
+// console.log(idade)
+
+// const idadeAluno = 17
+
+// const nomeAluna = "Manoela"
+
+// const triste = false
+
+// console.log(idadeAluno)
+// console.log(nomeAluna)
+// console.log(triste)
+
+// const pergunta = prompt("Qual o seu nome?")
+// console.log(pergunta)
+
+const nome = "Olivia"
+const sobrenome = "Dresch"
+const idade = 18
+console.log(nome, sobrenome, idade)
